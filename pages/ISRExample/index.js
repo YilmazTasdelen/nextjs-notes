dc0e52ff16cafa7aa6547d5dbaac790e58135035
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-07-02 18:01:33 +0300

    .

---
+++ pages/ISRExample/index.js
@@ -0,0 +1,32 @@
+function ISRExample({ products }) {
+    return (
+      <>
+        <h1>List of products</h1>
+        {products.map(product => {
+          return (
+            <div key={product.id}>
+              <h2>
+                {product.id} {product.title} {product.price}
+              </h2>
+              <hr />
+            </div>
+          )
+        })}
+      </>
+    )
+  }
+  
+  export default ISRExample
+  
+  export async function getStaticProps() {
+    console.log('Generating / Regenerating ProductList')
+    const response = await fetch('http://localhost:4000/products')
+    const data = await response.json()
+  
+    return {
+      props: {
+        products: data
+      },
+      revalidate: 30
+    }
+  }
