4fc35eb7d615de0cb3f54822b9a03d06f5a64ad8
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-07-02 16:56:50 +0300

    .

---
+++ pages/SSGWithDynamicParameters/posts/index.js
@@ -0,0 +1,39 @@
+import Link from 'next/link'
+/***
+ * SSG: static site generated.
+ * dev notes. example of SSG with dynamic parameter.
+ * 
+ * 
+ */
+function PostList({ posts }) {
+  return (
+    <>
+      <h1>List of Posts</h1>
+      {posts.map(post => {
+        return (
+          <div key={post.id}>
+            <Link href={`posts/${post.id}`}>
+              <h2>
+                {post.id} {post.title}
+              </h2>
+            </Link>
+            <hr />
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default PostList
+
+export async function getStaticProps() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const data = await response.json()
+
+  return {
+    props: {
+      posts: data.slice(0, 3)
+    }
+  }
+}
