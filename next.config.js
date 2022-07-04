cf8df6d24e97d27f588cf1601ce25dc166590aef
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-07-04 08:30:01 +0300

    .

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/about',
+        destination: '/',
+        permanent: false
+      }
+    ]
+  }
 }
+
 
 module.exports = nextConfig
---
+++ pages/comments/index.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+
+function CommentsPage() {
+    const [comments, setcomment] = useState([])
+    const [comment, setComment] = useState('')
+
+    const fetchComments = async () => {
+        const response = await fetch('/api/comments');
+        const data = await response.json()
+        setcomment(data)
+    }
+
+    const submitComment = async () => {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({ comment }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = await response.json()
+        console.log(data)
+    }
+
+const deleteComment = async (commentId) => {
+    const response = await fetch(`/api/comments/${commentId}`,{
+        method:'DELETE',
+    })
+    const data = await response.json()
+    console.log(data)
+    fetchComments()
+
+}
+
+    return (
+        <>
+            <div>
+                <input
+                    type='text'
+                    value={comment}
+                    onChange={e => setComment(e.target.value)}
+                />
+                <button onClick={submitComment}>Submit comment</button>
+            </div>
+            <hr />
+            <button onClick={fetchComments}>Load Comments</button>
+            {
+                comments.map(comment => {
+                    return (
+                        <div key={comment.id}>
+                            {comment.id} - {comment.text}
+                            <button onClick={()=>deleteComment(comment.id)}>Delete</button>
+                        </div>
+                    )
+                })
+            }
+        </>
+    )
+}
+
+export default CommentsPage;
