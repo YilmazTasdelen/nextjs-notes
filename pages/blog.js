e374414a6707eb8b99048d83998a947384cfd84d
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-07-05 20:21:09 +0300

    .

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import { getSession, useSession } from 'next-auth/client'
 
-const blog = () => {
-  return (
-    <div>blog</div>
-  )
+function Blog({ data }) {
+  const [session] = useSession()
+  console.log({ session })
+
+  return <h1>Blog page - {data}</h1>
 }
 
-export default blog
+export default Blog
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+  return {
+    props: {
+      data: session ? 'personalized':'not logined not free blogs'
+    }
+  }
+}
